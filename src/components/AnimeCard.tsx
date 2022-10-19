883946f4671579bd121ebb222c1cbd121f62dfc3
Keadeish <you@example.com>
2022-10-20 00:04:20 +0100

    changed site colour

--- src/components/AnimeCard.tsx
+++ src/components/AnimeCard.tsx
@@ -1,5 +1,4 @@
 import React, { useEffect, useState } from "react";
-import { animeStructure, randomAnimeStructure } from "../utils/animeStructure";
 import { FirstAnime } from "./FirstAnime";
 import { RandomAnime } from "./RandomAnime";
 import { SecondAnime } from "./SecondAnime";
@@ -7,7 +6,14 @@ import { ThirdAnime } from "./ThirdAnime";
 export function AnimeCard(): JSX.Element {
   return (
     <>
-      <FirstAnime /> <SecondAnime /> <ThirdAnime /> <RandomAnime />
+      <div className="container">
+        <div className="boarder">
+          <FirstAnime />
+          <SecondAnime />
+          <ThirdAnime />
+          <RandomAnime />
+        </div>
+      </div>
     </>
   );
 }
